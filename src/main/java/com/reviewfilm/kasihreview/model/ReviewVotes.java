6f2bd0643d3bfa9a0b953aae328eed7eb0f33490
Dzaky Renaldy <dzakyreviewfilm@gmail.com>
2025-10-26 16:23:36 +0700

    perbaiki error di reviewvotes

--- src/main/java/com/reviewfilm/kasihreview/model/ReviewVotes.java
+++ src/main/java/com/reviewfilm/kasihreview/model/ReviewVotes.java
@@ -1,9 +1,11 @@
 package com.reviewfilm.kasihreview.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+@Entity
 public class ReviewVotes {
     @Id
     private int voteId;
