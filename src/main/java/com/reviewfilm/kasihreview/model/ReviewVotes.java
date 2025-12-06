2fc96825011c484a954468bb1421fdeb76ef974f
Dzaky Renaldy <dzakyreviewfilm@gmail.com>
2025-12-06 21:10:48 +0700

    tambah gen value

--- src/main/java/com/reviewfilm/kasihreview/model/ReviewVotes.java
+++ src/main/java/com/reviewfilm/kasihreview/model/ReviewVotes.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -13,6 +15,7 @@ import jakarta.persistence.Table;
 @Table(name = "review_votes")
 public class ReviewVotes {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
     private int voteId;
 
     @Column(name = "vote_type", nullable = false)
