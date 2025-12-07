e7e4d51f379ba2abfd99728ab1acc6c15dca47d9
Dzaky Renaldy <dzakyreviewfilm@gmail.com>
2025-12-07 19:04:40 +0700

    tambah gen values di review

--- src/main/java/com/reviewfilm/kasihreview/model/Review.java
+++ src/main/java/com/reviewfilm/kasihreview/model/Review.java
@@ -8,6 +8,8 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -18,6 +20,7 @@ import jakarta.persistence.Table;
 @Table(name = "reviews")
 public class Review {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
     private int reviewId;
 
     private int rating;
