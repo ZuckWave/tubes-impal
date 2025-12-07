d24388a7c62292e52f2be45262ad2728b399fecf
Dzaky Renaldy <dzakyreviewfilm@gmail.com>
2025-12-07 17:27:35 +0700

    hapus unused import

--- src/main/java/com/reviewfilm/kasihreview/model/Movies.java
+++ src/main/java/com/reviewfilm/kasihreview/model/Movies.java
@@ -3,7 +3,6 @@ package com.reviewfilm.kasihreview.model;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
@@ -11,8 +10,6 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
